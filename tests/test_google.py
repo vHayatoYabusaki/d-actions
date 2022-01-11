8aee8142108349349055986fd7fbcc7815d1767a
薮崎 隼人 [ベリサーブ] <hayato.yabusaki@veriserve.co.jp>
2022-01-11 13:24:24 +0900

    test: add test_google.py

---
+++ tests/test_google.py
@@ -0,0 +1,9 @@
+import time
+
+from playwright.sync_api import Page
+
+
+class TestUserAgent:
+    def test_useragent(self, page: Page):
+        page.goto("http://whatsmyuseragent.org/")
+        time.sleep(5)
