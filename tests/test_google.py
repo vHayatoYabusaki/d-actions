9584e921f4b08dda5fba2e5ddfa57bbd7630bf97
薮崎 隼人 [ベリサーブ] <hayato.yabusaki@veriserve.co.jp>
2022-02-16 14:11:42 +0900

    Update test_google.py

--- tests/test_google.py
+++ tests/test_google.py
@@ -6,4 +6,4 @@ from playwright.sync_api import Page
 class TestUserAgent:
     def test_useragent(self, page: Page):
         page.goto("http://whatsmyuseragent.org/")
-        time.sleep(5)
+        time.sleep(10)
