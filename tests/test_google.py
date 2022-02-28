feed7601a7b95a3e9b41e00b64062baa9b474adf
薮崎 隼人 [ベリサーブ] <hayato.yabusaki@veriserve.co.jp>
2022-02-28 13:49:57 +0900

    Update test_google.py

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,7 +1,17 @@
 from playwright.sync_api import Page
+import pytest
 
 
 class TestGoogle:
     def test_google(self, page: Page):
         page.goto("https://www.google.com/")
         assert "google" in page.url
+
+    def test_fail_google(self, page: Page):
+        page.goto("https://www.google.com/")
+        assert "google" not in page.url
+
+    @pytest.mark.skip
+    def test_skip_google(self, page: Page):
+        page.goto("https://www.google.com/")
+        assert "google" not in page.url
