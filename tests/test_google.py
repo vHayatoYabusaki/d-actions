71f4eda55b03890ce35882407a7a07a2f89e64e0
薮崎 隼人 [ベリサーブ] <hayato.yabusaki@veriserve.co.jp>
2022-03-03 17:29:15 +0900

    Updated

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,11 +1,12 @@
-from playwright.sync_api import Page
+import allure
 import pytest
+from playwright.sync_api import Page
 
 
 class TestGoogle:
     def test_google(self, page: Page):
         page.goto("https://www.google.com/")
-        assert "google" in page.url
+        allure.attach(page.screenshot(), attachment_type=allure.attachment_type.PNG)
 
     def test_fail_google(self, page: Page):
         page.goto("https://www.google.com/")
