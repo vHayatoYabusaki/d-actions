5627038deb377d6f7819db57e477688a81f92f8c
薮崎 隼人 [ベリサーブ] <hayato.yabusaki@veriserve.co.jp>
2022-03-07 10:35:00 +0900

    Update test_runner_google.sh

--- tests/runners/test_runner_google.sh
+++ tests/runners/test_runner_google.sh
@@ -1,3 +1,3 @@
 RELATIVE_DIR=`dirname "$0"`
 
-poetry run pytest $RELATIVE_DIR/body/test_google.py --alluredir=allure-results
+poetry run pytest $RELATIVE_DIR/../body/test_google.py --alluredir=allure-results
